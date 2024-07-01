c95bacc69bbfbeda4eb17b1ca111a8f97d10f0b5
marzieh <marziehmonshizade@yahoo.com>
2024-07-01 14:18:15 +0330

    add layout: coding forms layout

--- src/components/layout/formLayout.tsx
+++ src/components/layout/formLayout.tsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React from "react";
 
 type FormLayoutProps = { children: React.ReactNode };
 
-
 const FormLayout = ({ children }: FormLayoutProps) => {
-    return (
-        <div>
-            {children}
-        </div>
-    )
-}
+  return (
+    <div
+      className="h-screen w-screen flex justify-center items-center bg-no-repeat bg-[#F5F5F5] bg-center bg-opacity-75"
+      style={{
+        backgroundImage: 'url("/images/pattern.svg")',
+      }}
+    >
+      {children}
+    </div>
+  );
+};
 
-export default FormLayout
+export default FormLayout;
