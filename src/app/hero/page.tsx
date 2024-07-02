21439e700af42a2137b4d8bfea24cff7a35aec65
marzieh <marziehmonshizade@yahoo.com>
2024-07-02 17:54:24 +0330

    refactor: refactor hero page

--- src/app/hero/page.tsx
+++ src/app/hero/page.tsx
@@ -1,37 +1,32 @@
-// "use client";
+import FormLayout from "@/components/layout/formLayout";
 import Image from "next/image";
 
-interface Props {}
-
-const Hero = (props: Props) => {
+const Hero = () => {
   return (
-    <div
-      className="h-screen w-screen flex justify-center items-center bg-no-repeat bg-auto bg-center bg-opacity-75"
-      style={{
-        backgroundImage: 'url("/images/backgroundImage.png")',
-      }}
-    >
-      <div className="flex flex-col justify-center items-center gap-4 w-[394px] h-[239px] mx-auto p-8 border border-slate-100 bg-center rounded-[12px] bg-white">
+    <FormLayout>
+      <div className="flex flex-col justify-center items-center gap-4 w-[394px] h-[239px] mx-auto p-8 border border-slate-100 bg-center rounded-[12px] bg-white shadow-sm shadow-[#E8E8E8]">
         <Image
           alt="heroIcon"
-          src="/images/heroIcon.png"
+          src="/images/heroIcon2.svg"
           width={72}
           height={72}
         />
-        <p className="text-[#9AD852]">ورود با موفقیت انجام شد</p>
-        <div className="flex justify-center items-center gap-4">
+        <p className="text-[#9AD852] font-medium text-base">
+          ورود با موفقیت انجام شد
+        </p>
+        <div className="flex justify-center items-center gap-2">
           <Image
             alt="heroIcon"
-            src="/images/spinner.png"
+            src="/images/Loading.svg"
             width={30}
             height={30}
           />
-          <p className="text-[#AAAAAA] text-xs">
+          <p className="text-[#AAAAAA] text-sm font-medium">
             در حال انتقال به صفحه اصلی ...
           </p>
         </div>
       </div>
-    </div>
+    </FormLayout>
   );
 };
 
