fac09e651d068e095e126fb2303c1f77f60ffb8a
marzieh <marziehmonshizade@yahoo.com>
2024-07-05 17:11:39 +0330

    refactor: refactor table of sizes

--- src/components/template/cut/tableOfSizes/table.tsx
+++ src/components/template/cut/tableOfSizes/table.tsx
@@ -1,3 +1,5 @@
+import { Sizes } from "@/data/sizes";
+
 function SizeTable() {
   return (
     <table className="min-w-full border-2 border-[#E3E3E3] text-center text-sm font-light text-surface dark:border-white/10 dark:text-white">
@@ -60,93 +62,42 @@ function SizeTable() {
         </tr>
       </thead>
       <tbody className="text-sm font-normal">
-        <tr className="odd:bg-white h-9 odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 dark:border-gray-700 border-b border-[#E3E3E3] dark:border-white/10">
-          <td className=" py-[9px] whitespace-nowrap border-e border-[#E3E3E3] dark:border-white/10">
-            0001
-          </td>
-          <td className="whitespace-nowrap border-e border-[#E3E3E3] dark:border-white/10">
-            10
-          </td>
-          <td className="whitespace-nowrap border-e border-[#E3E3E3] dark:border-white/10">
-            10
-          </td>
-          <td className="whitespace-nowrap border-e border-[#E3E3E3] dark:border-white/10">
-            10
-          </td>
-          <td className="whitespace-nowrap border-e border-[#E3E3E3] dark:border-white/10">
-            10
-          </td>
-          <td className="whitespace-nowrap border-e border-[#E3E3E3] dark:border-white/10">
-            10
-          </td>
-          <td className="whitespace-nowrap border-e border-[#E3E3E3] dark:border-white/10">
-            10
-          </td>
-          <td className="whitespace-nowrap border-e border-[#E3E3E3] dark:border-white/10">
-            10
-          </td>
-          <td className="text-start ps-4 whitespace-nowrap border-e border-[#E3E3E3] dark:border-white/10">
-            70
-          </td>
-        </tr>
-        <tr className="odd:bg-white h-9 odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 dark:border-gray-700 border-b border-[#E3E3E3] dark:border-white/10">
-          <td className="px-1 py-[9px] whitespace-nowrap border-e border-[#E3E3E3] dark:border-white/10">
-            0002
-          </td>
-          <td className="whitespace-nowrap border-e border-[#E3E3E3] dark:border-white/10">
-            10
-          </td>
-          <td className="whitespace-nowrap border-e border-[#E3E3E3] dark:border-white/10">
-            10
-          </td>
-          <td className="whitespace-nowrap border-e border-[#E3E3E3] dark:border-white/10">
-            10
-          </td>
-          <td className="whitespace-nowrap border-e border-[#E3E3E3] dark:border-white/10">
-            10
-          </td>
-          <td className="whitespace-nowrap border-e border-[#E3E3E3] dark:border-white/10">
-            10
-          </td>
-          <td className="whitespace-nowrap border-e border-[#E3E3E3] dark:border-white/10">
-            10
-          </td>
-          <td className="whitespace-nowrap border-e border-[#E3E3E3] dark:border-white/10">
-            10
-          </td>
-          <td className="text-start ps-4 whitespace-nowrap border-e border-[#E3E3E3] dark:border-white/10">
-            70
-          </td>
-        </tr>
-        <tr className="odd:bg-white h-9 odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 dark:border-gray-700 border-b border-[#E3E3E3] dark:border-white/10">
-          <td className="px-1 py-[9px] whitespace-nowrap border-e border-[#E3E3E3] dark:border-white/10">
-            0003
-          </td>
-          <td className="whitespace-nowrap border-e border-[#E3E3E3] dark:border-white/10">
-            10
-          </td>
-          <td className="whitespace-nowrap border-e border-[#E3E3E3] dark:border-white/10">
-            10
-          </td>
-          <td className="whitespace-nowrap border-e border-[#E3E3E3] dark:border-white/10">
-            10
-          </td>
-          <td className="whitespace-nowrap border-e border-[#E3E3E3] dark:border-white/10">
-            10
-          </td>
-          <td className="whitespace-nowrap border-e border-[#E3E3E3] dark:border-white/10">
-            10
-          </td>
-          <td className="whitespace-nowrap border-e border-[#E3E3E3] dark:border-white/10">
-            10
-          </td>
-          <td className="whitespace-nowrap border-e border-[#E3E3E3] dark:border-white/10">
-            10
-          </td>
-          <td className="text-start ps-4 whitespace-nowrap border-e border-[#E3E3E3] dark:border-white/10">
-            70
-          </td>
-        </tr>
+        {Sizes.map((size) => {
+          return (
+            <tr
+              key={size.id}
+              className="odd:bg-white h-9 odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 dark:border-gray-700 border-b border-[#E3E3E3] dark:border-white/10"
+            >
+              <td className=" py-[9px] whitespace-nowrap border-e border-[#E3E3E3] dark:border-white/10">
+                000{size.id}
+              </td>
+              <td className="whitespace-nowrap border-e border-[#E3E3E3] dark:border-white/10">
+                {size.XS}
+              </td>
+              <td className="whitespace-nowrap border-e border-[#E3E3E3] dark:border-white/10">
+                {size.S}
+              </td>
+              <td className="whitespace-nowrap border-e border-[#E3E3E3] dark:border-white/10">
+                {size.M}
+              </td>
+              <td className="whitespace-nowrap border-e border-[#E3E3E3] dark:border-white/10">
+                {size.L}
+              </td>
+              <td className="whitespace-nowrap border-e border-[#E3E3E3] dark:border-white/10">
+                {size.XL}
+              </td>
+              <td className="whitespace-nowrap border-e border-[#E3E3E3] dark:border-white/10">
+                {size.XXL}
+              </td>
+              <td className="whitespace-nowrap border-e border-[#E3E3E3] dark:border-white/10">
+                {size.XXXl}
+              </td>
+              <td className="text-start ps-4 whitespace-nowrap border-e border-[#E3E3E3] dark:border-white/10">
+                {size.total}
+              </td>
+            </tr>
+          );
+        })}
         <tr className="border-b-none font-medium bg-[#F5F5F5] border-[#E3E3E3] dark:border-white/10">
           <td className="p-2 whitespace-nowrap w-9 text-[#2B5BB7] text-base font-medium border-e border-[#E3E3E3] dark:border-white/10">
             جمع کل
