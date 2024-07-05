28ecb8a7e1f709c4625347cd6f29bc53f3d072d0
marzieh <marziehmonshizade@yahoo.com>
2024-07-05 17:10:53 +0330

    refactor: refactor cutting table

--- src/components/template/cut/cuttingTable/table.tsx
+++ src/components/template/cut/cuttingTable/table.tsx
@@ -1,3 +1,5 @@
+import { Cuts } from "@/data/cuts";
+
 function CuttingTable() {
   return (
     <>
@@ -31,51 +33,31 @@ function CuttingTable() {
           </tr>
         </thead>
         <tbody className="text-sm font-normal">
-          <tr className="odd:bg-white h-9 odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 dark:border-gray-700 border-b border-[#E3E3E3] dark:border-white/10">
-            <td className="whitespace-nowrap border-e border-[#E3E3E3] px-3 py-[9px] dark:border-white/10">
-              00001
-            </td>
-            <td className="whitespace-nowrap px-3 py-[9px] dark:border-white/10">
-              سبز سرخ آبی
-            </td>
-            <td className="whitespace-nowrap px-3 py-[9px] dark:border-white/10">
-              00006
-            </td>
-            <td className="whitespace-nowrap border-e border-[#E3E3E3] px-3 py-[9px] dark:border-white/10">
-              000001
-            </td>
-            <td className="whitespace-nowrap px-3 py-[9px]">1</td>
-          </tr>
-          <tr className="odd:bg-white h-9 odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 dark:border-gray-700 border-b border-[#E3E3E3] dark:border-white/10">
-            <td className="whitespace-nowrap border-e border-[#E3E3E3] px-3 py-[9px] dark:border-white/10">
-              00002
-            </td>
-            <td className="whitespace-nowrap px-3 py-[9px] dark:border-white/10">
-              مشکی
-            </td>
-            <td className="whitespace-nowrap px-3 py-[9px] dark:border-white/10">
-              00006
-            </td>
-            <td className="whitespace-nowrap border-e border-[#E3E3E3] px-3 py-[9px] dark:border-white/10">
-              000001
-            </td>
-            <td className="whitespace-nowrap px-3 py-[9px]">1</td>
-          </tr>
-          <tr className="odd:bg-white h-9 odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 dark:border-gray-700 border-b border-[#E3E3E3] dark:border-white/10">
-            <td className="whitespace-nowrap border-e border-[#E3E3E3] px-3 py-[9px] dark:border-white/10">
-              00003
-            </td>
-            <td className="whitespace-nowrap px-3 py-[9px] dark:border-white/10">
-              آبی
-            </td>
-            <td className="whitespace-nowrap px-3 py-[9px] dark:border-white/10">
-              00006
-            </td>
-            <td className="whitespace-nowrap border-e border-[#E3E3E3] px-3 py-[9px] dark:border-white/10">
-              000001
-            </td>
-            <td className="whitespace-nowrap px-3 py-[9px]">1</td>
-          </tr>
+          {Cuts.map((cut) => {
+            return (
+              <tr
+                key={cut.id}
+                className="odd:bg-white h-9 odd:dark:bg-gray-900 even:bg-gray-50 even:dark:bg-gray-800 dark:border-gray-700 border-b border-[#E3E3E3] dark:border-white/10"
+              >
+                <td className="whitespace-nowrap border-e border-[#E3E3E3] px-3 py-[9px] dark:border-white/10">
+                  0000{cut.id}
+                </td>
+                <td className="whitespace-nowrap px-3 py-[9px] dark:border-white/10">
+                  {cut.color}
+                </td>
+                <td className="whitespace-nowrap px-3 py-[9px] dark:border-white/10">
+                  {cut.colorCode}
+                </td>
+                <td className="whitespace-nowrap border-e border-[#E3E3E3] px-3 py-[9px] dark:border-white/10">
+                  {cut.qualityCode}
+                </td>
+                <td className="whitespace-nowrap px-3 py-[9px]">
+                  {cut.numberOfHeight}
+                </td>
+              </tr>
+            );
+          })}
+
           <tr className="bg-[#F5F5F5] border-2 border-t-0 border-[#E3E3E3] border-b dark:border-white/10">
             <td className="whitespace-nowrap px-3 py-[9px] dark:border-white/10"></td>
             <td className="whitespace-nowrap px-3 py-[9px] text-[#2B5BB7] text-base font-medium dark:border-white/10">
---
+++ src/data/cuts.ts
@@ -0,0 +1,23 @@
+export const Cuts = [
+  {
+    id: 1,
+    color: "سبز سرخ آبی",
+    colorCode: "00006",
+    qualityCode: "000001",
+    numberOfHeight: 1,
+  },
+  {
+    id: 2,
+    color: "مشکی",
+    colorCode: "00006",
+    qualityCode: "000001",
+    numberOfHeight: 1,
+  },
+  {
+    id: 3,
+    color: "آبی",
+    colorCode: "00006",
+    qualityCode: "000001",
+    numberOfHeight: 1,
+  },
+];
